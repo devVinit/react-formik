90375653152ffc136558c6c13144e537e2bc1484
Vinit <vinit.kumar@binaryinformatics.com>
2019-07-09 22:17:41 +0530

    form init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Header, Input, Form, Grid } from 'semantic-ui-react';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid centered columns={2}>
+      <Grid.Row centered>
+        <Grid.Column>
+          <Header.Content as="h1">
+            React Formik
+          </Header.Content>
+          <Form>
+            <Form.Field>
+              <label>Name</label>
+              <Input placeholder='Name' />
+            </Form.Field>
+            <Form.Field>
+              <label>Phone Number</label>
+              <Input placeholder='Mobile' type='number' min={0} />
+            </Form.Field>
+            <Form.Field>
+              <label>Address</label>
+              <Input placeholder='Address' />
+            </Form.Field>
+            <Form.Field>
+              <label>Occupation</label>
+              <Input placeholder='Occupation' />
+            </Form.Field>
+            <Form.Button>
+              Submit
+          </Form.Button>
+          </Form>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   );
 }
 
